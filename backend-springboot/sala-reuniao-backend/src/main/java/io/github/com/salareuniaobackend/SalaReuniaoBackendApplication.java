61922cc4e7f6e5619d165d9d2748825aa6fc407b
Flávio Leandro Pereira <doflavio@gmail.com>
2021-07-20 22:02:07 -0300

    Commit inicial - backend-springboot

---
+++ backend-springboot/sala-reuniao-backend/src/main/java/io/github/com/salareuniaobackend/SalaReuniaoBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.com.salareuniaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaReuniaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaReuniaoBackendApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/sala-reuniao-backend/src/test/java/io/github/com/salareuniaobackend/SalaReuniaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.salareuniaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
